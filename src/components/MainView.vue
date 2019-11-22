49fb878315f13386e4d331ec5a040e99d8febd9a
Sean Wong <seanwong24@live.com>
2019-11-22 19:58:04 +0000

    modify the app title and add a file upload button

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -2,18 +2,30 @@
   <ion-app>
     <ion-header>
       <ion-toolbar>
-        <ion-title>{{title}}</ion-title>
+        <ion-title>{{title}} - {{datasetName}}</ion-title>
+        <ion-buttons slot="end">
+          <ion-button title="Upload Dataset File" @click="upload()">
+            <ion-icon slot="icon-only" name="cloud-upload" size="large"></ion-icon>
+            <input
+              type="file"
+              id="file-upload-input"
+              accept=".json"
+              @input="fileUploaded()"
+              ref="fileUploadInput"
+            />
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content fixed id="main-content">
       <ion-grid>
         <ion-row>
           <ion-col size="9">
-            <VisView/>
+            <VisView />
           </ion-col>
           <ion-col size="3">
             <ion-card>
-              <ControlView/>
+              <ControlView />
             </ion-card>
           </ion-col>
         </ion-row>
@@ -25,15 +37,60 @@
 <script>
 import VisView from "./VisView";
 import ControlView from "./ControlView";
+import { addIcons } from "ionicons";
+import { cloudUpload } from "ionicons/icons";
+
+addIcons({
+  "md-cloud-upload": cloudUpload.md,
+  "ios-cloud-upload": cloudUpload.ios
+});
 
 export default {
   name: "MainView",
-  props: {
-    title: String
-  },
   components: {
     VisView,
     ControlView
+  },
+  props: {
+    title: String
+  },
+  data: () => ({
+    datasetFile: File,
+    dataset: null
+  }),
+  computed: {
+    datasetName: {
+      get() {
+        const fileName = this.datasetFile.name.substring(
+          0,
+          this.datasetFile.name.lastIndexOf(".")
+        );
+        return this.datasetFile && this.dataset
+          ? fileName
+          : "No Dataset Loaded";
+      }
+    }
+  },
+  methods: {
+    upload() {
+      this.$refs.fileUploadInput.click();
+    },
+    fileUploaded() {
+      this.datasetFile = this.$refs.fileUploadInput.files[0];
+      if (
+        this.datasetFile &&
+        this.datasetFile.name.split(".").pop() === "json"
+      ) {
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+          const fileContent = fileReader.result;
+          this.dataset = JSON.parse(fileContent);
+        };
+        fileReader.readAsText(this.datasetFile);
+      } else {
+        this.dataset = null;
+      }
+    }
   }
 };
 </script>
@@ -57,5 +114,9 @@ ion-row {
 ion-card {
   height: 100%;
   margin: 0;
+}
+
+#file-upload-input {
+  display: none;
 }
 </style>
